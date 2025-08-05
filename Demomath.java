504c3e84cd15b8200ce0c2f0c15a8e3fa41514ab
TT Ho <oscoao@gmail.com>
2025-08-05 13:26:37 +0800

    Update Demomath.java

---
+++ Demomath.java
@@ -0,0 +1,16 @@
+public class DemoMath {
+  public ststic void main (String[] args) {
+    int x = 5;
+    int y = 9;
+
+  System.out.println(x + y); // 14 (int) -> "14"
+    System.out.println(x - y);
+    System.out.println(x * y);
+    System.out.println(x / y);
+
+
+
+    
+  // try +,-,*,/
+  }
+}
