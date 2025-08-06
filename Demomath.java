b088195d3f5a5bc89d8f2453a1baf83e7279451b
TT.Ho <oscoao@gmail.com>
2025-08-06 10:08:03 +0800

    hello

--- Demomath.java
+++ Demomath.java
@@ -4,14 +4,7 @@ public class DemoMath {
     int y = 9;
 
   System.out.println(x + y); // 14 (int) -> "14"
-    System.out.println(x - y);
-    System.out.println(x * y);
-    System.out.println(x / y);
-
-
-
     
   // try +,-,*,/
   }
 }
-
---
+++ Demomath2.java
@@ -0,0 +1,17 @@
+public class Demomath2 {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 9;
+
+    System.out.println(x + y); // 14 (int) -> "14"
+    System.out.println(x - y); // -4 (int) -> "-4"
+    System.out.println(x * y); // 45 (int) -> "45"
+    System.out.println(x / y); //
+  }
+}
+public static void main1 (String[] args) {}
+public static void main2 (String[] args) {}
+public static void main3 (String[] args) {}
+public static void main4 (String[] args) {}
+public static void main5 (String[] args) {}
+public static void main6 
